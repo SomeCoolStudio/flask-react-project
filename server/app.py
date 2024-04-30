1ac8e4b1c227a5270a6eac0c7518a17681c523f4
SomeCoolStudio <47907012+SomeCoolStudio@users.noreply.github.com>
2024-04-29 18:30:31 -0700

    CRUD operations working in flask with ElephantSQL

--- server/app.py
+++ server/app.py
@@ -1,20 +1,79 @@
+import os
+
 from flask import Flask, jsonify
-from flask_cors import CORS
+from flask_smorest import Api
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+from dotenv import load_dotenv
 
-app = Flask(__name__)
-cors = CORS(app, origins='*')
+from db import db
+from blocklist import BLOCKLIST
+import models
 
-@app.route("/api/users", methods=['GET'])
-def users():
-    return jsonify(
-        {
-            "users":[
-                'los',
-                'loso',
-                'lobo'
-            ]
-        }
+from resources.item import bp as ItemBlueprint
+from resources.store import bp as StoreBlueprint
+from resources.user import bp as UserBlueprint
+
+
+def create_app(db_url=None):
+    app = Flask (__name__) 
+    load_dotenv()
+
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    api = Api(app)
+
+    app.config["JWT_SECRET_KEY"] = "Temporary Secret Key"
+    jwt = JWTManager(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+    
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401,
+        )
+
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (jsonify(
+            {
+                "description": "The token is not fresh.",
+                "error":"fresh_token_required"
+            }
+        ),
+        401,
     )
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (jsonify({"message": "The token has expired.", "error": "token_expired"}), 401,)
+    
+    @jwt.expired_token_loader
+    def invalid_token_callback(error):
+        return (jsonify({"message": "Signature verification failed.", "error": "invalid_token"}), 401,)
+    
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (jsonify({"message": "Request does not contain an access token.", "error": "authorization_required"}), 401,)
+
+
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(UserBlueprint)
+
+    return app
---
+++ server/models/__init__.py
@@ -0,0 +1,3 @@
+from models.store_model import StoreModel
+from models.item_model import ItemModel
+from models.user_model import UserModel
